67fc9b120664ffeaad9646fd85b5c773be839fc5
IrvinTarlao <irvin@millionvictories.com>
2020-09-29 12:50:37 +0200

    dev

---
+++ src/components/Frame.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="frame">
+        <div class="keyboard">
+            <Key v-for="keyNote in keysList" v-bind:keyNote="keyNote" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Key from "./Key";
+
+export default {
+    name: "frame",
+    props: {
+        msg: String,
+    },
+    components: {
+        Key,
+    },
+    data() {
+        return {
+            keysList: [
+                { note: "F", keyboard: "f", color: "white", frequency: 349.23 },
+                { note: "G", keyboard: "g", color: "white", frequency: 392 },
+                { note: "A", keyboard: "h", color: "white", frequency: 440 },
+                { note: "B", keyboard: "j", color: "white", frequency: 493.88 },
+                { note: "C", keyboard: "k", color: "white", frequency: 523.25 },
+                { note: "D", keyboard: "l", color: "white", frequency: 587.33 },
+                { note: "E", keyboard: "m", color: "white", frequency: 659.25 },
+                { note: "F#", align: "flex-end", keyboard: "t", color: "black", frequency: 369.99 },
+                { note: "G#", align: "center", keyboard: "y", color: "black", frequency: 415.3 },
+                { note: "A#", align: "flex-start", keyboard: "u", color: "black", frequency: 466.16 },
+                { note: "C#", align: "flex-end", keyboard: "o", color: "black", frequency: 554.37 },
+                { note: "D#", align: "flex-start", keyboard: "p", color: "black", frequency: 622.25 },
+            ],
+        };
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.frame {
+    background-color: rgb(61, 61, 61);
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.keyboard {
+    width: 90%;
+    height: 50%;
+    border: 2px solid black;
+    background-color: red;
+    display: flex;
+}
+</style>
---
+++ src/components/Key.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="whiteKey">
+        <div class="" @keydown="objTest">{{this.props.keyNote}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "key",
+    data() {
+        return {};
+    },
+    props: {
+        keyNote: Object,
+    },
+    methods: {
+        debug(event) {
+            console.log(this.props.keyNote);
+        },
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.whiteKey {
+    background-color: white;
+    border: 2px solid grey;
+    width: calc(100% / 8);
+    height: 100%;
+}
+</style>
